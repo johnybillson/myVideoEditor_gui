aba56eb881d6c4cd727831bdc1d57f420a13ba07
johnybillson <vincent.guigue@epitech.eu>
2018-07-23 22:45:39 +0200

    Initial commit for myVideoEditor gui

---
+++ myVideoEditor_gui/Main.vb
@@ -0,0 +1,6 @@
+﻿Public Class Main
+
+    Private Sub Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        myVideoEditor.AboutBox()
+    End Sub
+End Class
